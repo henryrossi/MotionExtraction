b0e2f69a3d84bd4b9f575e6a63de0b3cd4728f13
henryrossi <henry.rossi219@gmail.com>
2024-01-17 12:16:38 -0800

    renamed i/oformat_context

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
 
 typedef struct VideoStreamCodec {
     // stream index?
@@ -58,6 +59,9 @@ int process_video(AVFormatContext *iformat_context, AVFormatContext *oformat_con
 
         video_codec->frame->pts = video_codec->frame->best_effort_timestamp;
 
+        // change frame
+        // struct SwsContext *sws_ctx = sws_getContext();
+
         ret = avcodec_send_frame(video_codec->encoderContext, video_codec->frame);
         if (ret < 0) {
             fprintf(stderr, "ERROR:   Failed sending frame to encoder, (error: %s)\n", av_err2str(ret));
@@ -77,31 +81,31 @@ int main(int argc, char *argv[]) {
     //     return -1;
     // }
 
-    AVFormatContext *inputFormatContext = NULL;
+    AVFormatContext *iformat_context = NULL;
 
-    int ret = avformat_open_input(&inputFormatContext, "pong.mov", NULL, NULL);
+    int ret = avformat_open_input(&iformat_context, "pong.mov", NULL, NULL);
     if (ret < 0) {
         fprintf(stderr, "ERROR:   Failed to open input file\n");
         return ret;
     }
 
-    ret = avformat_find_stream_info(inputFormatContext, NULL);
+    ret = avformat_find_stream_info(iformat_context, NULL);
     if (ret < 0) {
         fprintf(stderr, "ERROR:   Failed to read input file and find stream information\n");
         return ret;
     }
 
-    av_dump_format(inputFormatContext, 0, "pong.mov", 0);
+    av_dump_format(iformat_context, 0, "pong.mov", 0);
 
-    AVFormatContext *outputFormatContext = NULL;
-    ret = avformat_alloc_output_context2(&outputFormatContext, NULL, NULL, "newtest.mov");
+    AVFormatContext *oformat_context = NULL;
+    ret = avformat_alloc_output_context2(&oformat_context, NULL, NULL, "newtest.mov");
     if (ret < 0){
         fprintf(stderr, "ERROR:   Failed to allocate output context\n");
         return ret;
     }
 
-    if (!(outputFormatContext->oformat->flags & AVFMT_NOFILE)) {
-        ret = avio_open(&outputFormatContext->pb, "newtest.mov", AVIO_FLAG_WRITE);
+    if (!(oformat_context->oformat->flags & AVFMT_NOFILE)) {
+        ret = avio_open(&oformat_context->pb, "newtest.mov", AVIO_FLAG_WRITE);
         if (ret < 0) {
             fprintf(stderr, "ERROR:   Could not open output file '%s'", "newtest.mov");
             return ret;
@@ -122,25 +126,25 @@ int main(int argc, char *argv[]) {
 
     int video_stream = -1;
 
-    for (int i = 0; i < inputFormatContext->nb_streams; i++) {
-        AVStream *stream = avformat_new_stream(outputFormatContext, NULL);
+    for (int i = 0; i < iformat_context->nb_streams; i++) {
+        AVStream *stream = avformat_new_stream(oformat_context, NULL);
         if (stream == NULL) {
             fprintf(stderr, "ERROR:   Failed to add a new stream to the output file\n");
             return -1;
         }
 
-        ret = avcodec_parameters_copy(stream->codecpar, inputFormatContext->streams[i]->codecpar);
+        ret = avcodec_parameters_copy(stream->codecpar, iformat_context->streams[i]->codecpar);
         if (ret < 0) {
             fprintf(stderr, "ERROR:   Failed to copy codec params form the input to the output stream\n");
             return ret;
         }
 
-        if (inputFormatContext->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
+        if (iformat_context->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
             video_stream = i;
         }
     }
 
-    const AVCodec *decoder = avcodec_find_decoder(inputFormatContext->streams[video_stream]->codecpar->codec_id);
+    const AVCodec *decoder = avcodec_find_decoder(iformat_context->streams[video_stream]->codecpar->codec_id);
     if (decoder == NULL) {
         fprintf(stderr, "ERROR:   Couldn't find the encoder/decoder\n");
         return -1;
@@ -152,7 +156,7 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
-    ret = avcodec_parameters_to_context(decoderContext, inputFormatContext->streams[video_stream]->codecpar);
+    ret = avcodec_parameters_to_context(decoderContext, iformat_context->streams[video_stream]->codecpar);
     if (ret < 0) {
         fprintf(stderr, "ERROR:   Failed to fill the decoder context based on the values from codec parameters\n");
         return ret;
@@ -167,7 +171,7 @@ int main(int argc, char *argv[]) {
     videoStreamCodec->decoderContext = decoderContext;
 
 
-    const AVCodec *encoder = avcodec_find_encoder(inputFormatContext->streams[video_stream]->codecpar->codec_id);
+    const AVCodec *encoder = avcodec_find_encoder(iformat_context->streams[video_stream]->codecpar->codec_id);
     if (encoder == NULL) {
         fprintf(stderr, "ERROR:   Couldn't find the encoder/decoder\n");
         return -1;
@@ -179,15 +183,15 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
-    ret = avcodec_parameters_to_context(encoderContext, inputFormatContext->streams[video_stream]->codecpar);
+    ret = avcodec_parameters_to_context(encoderContext, iformat_context->streams[video_stream]->codecpar);
     if (ret < 0) {
         fprintf(stderr, "ERROR:   Failed to fill the encoder context based on the values from codec parameters\n");
         return ret;
     }
 
-    encoderContext->time_base = av_inv_q(av_guess_frame_rate(inputFormatContext, inputFormatContext->streams[video_stream], NULL));
+    encoderContext->time_base = av_inv_q(av_guess_frame_rate(iformat_context, iformat_context->streams[video_stream], NULL));
 
-    if (outputFormatContext->oformat->flags & AVFMT_GLOBALHEADER)
+    if (oformat_context->oformat->flags & AVFMT_GLOBALHEADER)
         encoderContext->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
 
     ret = avcodec_open2(encoderContext, encoder, NULL);
@@ -196,7 +200,7 @@ int main(int argc, char *argv[]) {
         return ret;
     }
 
-    ret = avcodec_parameters_from_context(outputFormatContext->streams[video_stream]->codecpar, encoderContext);
+    ret = avcodec_parameters_from_context(oformat_context->streams[video_stream]->codecpar, encoderContext);
     if (ret < 0) {
         fprintf(stderr, "ERROR:   Failed avcodec_parameters_from_context()\n");
         return ret;
@@ -204,9 +208,9 @@ int main(int argc, char *argv[]) {
 
     videoStreamCodec->encoderContext = encoderContext;
 
-    av_dump_format(outputFormatContext, 0, "newtest.mov", 1);
+    av_dump_format(oformat_context, 0, "newtest.mov", 1);
 
-    ret = avformat_write_header(outputFormatContext, NULL);
+    ret = avformat_write_header(oformat_context, NULL);
     if (ret < 0) {
         fprintf(stderr, "ERROR:   Failed to write header to the output file\n");
         return ret;
@@ -218,21 +222,21 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
-    while (av_read_frame(inputFormatContext, packet) >= 0) {
+    while (av_read_frame(iformat_context, packet) >= 0) {
         int streamIndex = packet->stream_index;
 
-        if (inputFormatContext->streams[streamIndex]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
+        if (iformat_context->streams[streamIndex]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
             ret = avcodec_send_packet(videoStreamCodec->decoderContext, packet);
             if (ret < 0) {
                 fprintf(stderr, "ERROR:   Failed sending packet to decoder\n");
                 return ret;
             }
 
-            ret = process_video(inputFormatContext, outputFormatContext, videoStreamCodec, packet, streamIndex);
+            ret = process_video(iformat_context, oformat_context, videoStreamCodec, packet, streamIndex);
             if (ret < 0) return ret;
         } 
         else {
-            ret = mux(inputFormatContext, outputFormatContext, packet, streamIndex);
+            ret = mux(iformat_context, oformat_context, packet, streamIndex);
             if (ret < 0) return ret;
         }
 
@@ -244,7 +248,7 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "ERROR:   Failed to flush decoder\n");
         return ret;
     }
-    ret = process_video(inputFormatContext, outputFormatContext, videoStreamCodec, packet, video_stream);
+    ret = process_video(iformat_context, oformat_context, videoStreamCodec, packet, video_stream);
     if (ret < 0) return ret;
     
     ret = avcodec_send_frame(videoStreamCodec->encoderContext, NULL);
@@ -252,10 +256,10 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "ERROR:   Failed sending frame to encoder, (error: %s)\n", av_err2str(ret));
         return ret;
     }
-    ret = encode_video(inputFormatContext, outputFormatContext, videoStreamCodec, packet, video_stream);
+    ret = encode_video(iformat_context, oformat_context, videoStreamCodec, packet, video_stream);
     if (ret < 0) return ret;
 
-    ret = av_write_trailer(outputFormatContext);
+    ret = av_write_trailer(oformat_context);
     if (ret < 0) {
         fprintf(stderr, "ERROR:   Couldn't wirte output file trailer\n");
         return ret;
@@ -263,10 +267,10 @@ int main(int argc, char *argv[]) {
 
     av_packet_free(&packet);
     av_free(videoStreamCodec);
-    if (outputFormatContext && !(outputFormatContext->oformat->flags & AVFMT_NOFILE))
-        avio_closep(&outputFormatContext->pb);
-    avformat_free_context(outputFormatContext);
-    avformat_close_input(&inputFormatContext);
+    if (oformat_context && !(oformat_context->oformat->flags & AVFMT_NOFILE))
+        avio_closep(&oformat_context->pb);
+    avformat_free_context(oformat_context);
+    avformat_close_input(&iformat_context);
 
     return 0;
 }
