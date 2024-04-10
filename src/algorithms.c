b987328c144ddf0887b31898a94ac98b8f60a176
Henry Rossi <henry.rossi219@gmail.com>
2024-04-10 16:55:27 -0700

    implemented deep_copy_frame

--- src/algorithms.c
+++ src/algorithms.c
@@ -1,6 +1,33 @@
 #include "algorithms.h"
 #include "types.h"
 
+AVFrame *deep_copy_frame(AVFrame *src) {
+    AVFrame *new_frame = av_frame_alloc();
+    if (new_frame == NULL) {
+        return NULL;
+    }
+
+    new_frame->format = src->format;
+    new_frame->width = src->width;
+    new_frame->height = src->height;
+
+    int ret = av_frame_get_buffer(new_frame, 0);
+    if (ret < 0) {
+        return NULL;
+    }
+
+    ret = av_frame_copy(new_frame, src);
+    if (ret < 0) {
+        av_frame_free(&new_frame);
+        return NULL;
+    }
+
+    new_frame->pts = src->pts;
+    new_frame->pkt_dts = src->pkt_dts;
+
+    return new_frame;
+}
+
 int write_inverted_frame_yuv420p(VideoCodec *video_codec) {
     for (int i = 0; i < 3; i++) {
         printf("The frame->data[%d] has a width of %d, height of %d, and linesize of %d\n",
@@ -50,6 +77,11 @@ int overlay_frames_yuv420p(VideoCodec *video_codec) {
         if (ret < 0) return ret;
     }
 
+    AVFrame *new_frame = deep_copy_frame(video_codec->frame);
+    if (new_frame == NULL) {
+        return -1;
+    }
+
     /* Y */
     for (int y = 0; y < video_codec->frame->height; y++) {
         for (int x = 0; x < video_codec->frame->linesize[0]; x++) {
@@ -58,7 +90,7 @@ int overlay_frames_yuv420p(VideoCodec *video_codec) {
             if (byte > 255) {
                 byte = 255;
             }
-            video_codec->frame->data[0][y * video_codec->frame->linesize[0] + x] = byte;
+            new_frame->data[0][y * video_codec->frame->linesize[0] + x] = byte;
         }
     }
 
@@ -70,16 +102,20 @@ int overlay_frames_yuv420p(VideoCodec *video_codec) {
             if (byte > 255) {
                 byte = 255;
             }   
-            video_codec->frame->data[1][y * video_codec->frame->linesize[1] + x] = byte;
+            new_frame->data[1][y * video_codec->frame->linesize[1] + x] = byte;
 
             byte = (video_codec->inverted_data[2][y * video_codec->frame->linesize[2] + x] +
                 video_codec->frame->data[2][y * video_codec->frame->linesize[2] + x]) / 2;
             if (byte > 255) {
                 byte = 255;
             }
-            video_codec->frame->data[2][y * video_codec->frame->linesize[2] + x] = byte;
+            new_frame->data[2][y * video_codec->frame->linesize[2] + x] = byte;
         }
     }
+
+    av_frame_unref(video_codec->frame);
+    av_frame_free(&video_codec->frame);
+    video_codec->frame = new_frame;
 
     return ret;
 }
